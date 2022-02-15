81491a4405ec4c704f7e7edb74deeb438dfec586
Kharalos <ounal97@gmail.com>
2022-02-15 20:43:51 +0300

    First Challange - ProgressBar

--- Assets/Challenges/1. Basic Progress Bar/Scripts/ProgressBar.cs
+++ Assets/Challenges/1. Basic Progress Bar/Scripts/ProgressBar.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Challenges._1._Basic_Progress_Bar.Scripts
 {
@@ -26,6 +29,9 @@ namespace Challenges._1._Basic_Progress_Bar.Scripts
             Progress,
             NoText
         }
+        [SerializeField] private Image fillBar;
+        [SerializeField] private TMPro.TextMeshProUGUI percentageText;
+        private float desiredValue;
         
         /// <summary>
         /// These settings below must function
@@ -37,7 +43,8 @@ namespace Challenges._1._Basic_Progress_Bar.Scripts
         private ProgressSnapOptions snapOptions;
         [SerializeField]
         private TextPosition textPosition;
-        
+        private bool smoothChange, snapText;
+        float changeValue;
         
         
         /// <summary>
@@ -46,6 +53,11 @@ namespace Challenges._1._Basic_Progress_Bar.Scripts
         /// <param name="value">Must be in range [0,1]</param>
         public void ForceValue(float value)
         {
+            changeValue = value;
+
+            SetTextPosition();
+
+            desiredValue = value;
         }
 
         /// <summary>
@@ -55,7 +67,55 @@ namespace Challenges._1._Basic_Progress_Bar.Scripts
         /// <param name="speedOverride">Will override the base speed if one is given</param>
         public void SetTargetValue(float value, float? speedOverride = null)
         {
+            SetTextPosition();
+
+            if((value < desiredValue && snapOptions == ProgressSnapOptions.SnapToLowerValue) || (value > desiredValue && snapOptions == ProgressSnapOptions.SnapToHigherValue)){
+                changeValue = value;
+            }
+
+            desiredValue = value;
         }
 
+        /// <summary>
+        /// Update is called every frame, if the MonoBehaviour is enabled.
+        /// </summary>
+        void Update()
+        {
+            changeValue = Mathf.Lerp(changeValue, desiredValue, baseSpeed * Time.deltaTime);
+
+            percentageText.text = Mathf.Round(changeValue * 100).ToString() + "%";
+
+            fillBar.rectTransform.localScale = new Vector2(changeValue, 1f);
+
+            if(snapText){
+                percentageText.alignment = TMPro.TextAlignmentOptions.MidlineRight;
+                float percentageWidth = (transform.parent.GetComponent<RectTransform>().sizeDelta.x * 0.5f/*420*/) * (1 - Mathf.Clamp(changeValue, 0.15f,1f));
+                percentageText.rectTransform.offsetMax = new Vector2(-percentageWidth, 0);
+            }
+        }
+
+        private void SetTextPosition(){
+            percentageText.enabled = true;
+
+            snapText = false;
+
+            percentageText.alignment = TMPro.TextAlignmentOptions.CenterGeoAligned;
+
+            switch (textPosition) 
+            {
+                case TextPosition.BarCenter:
+                    percentageText.rectTransform.anchoredPosition3D = this.GetComponent<RectTransform>().anchoredPosition3D;
+                    break;
+
+                case TextPosition.Progress:
+                    snapText = true;
+                    break;
+
+                //No Text
+                default:
+                    percentageText.enabled = false;
+                    break;
+            }
+        }
     }
 }
